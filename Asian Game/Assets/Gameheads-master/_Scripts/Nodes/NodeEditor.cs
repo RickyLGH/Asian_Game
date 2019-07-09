d48fc86ff297cf784bc93bb304b99dc590495d7b
RickyLGH <liuricky38@gmail.com>
2019-07-09 15:47:24 -0700

    Base System (Nodes not done)

---
+++ Asian Game/Assets/Gameheads-master/_Scripts/Nodes/NodeEditor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NodeEditor : EditorWindow
+{
+    public static void DrawNodeCurve(Rect Start, Rect end);
+}
---
+++ Asian Game/Assets/Gameheads-master/_Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float minPosition;
+    public float maxPosition;
+
+    public float moveSpeed = 1.0f;
+
+    public float interactionRadius = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the player, clamping them to within the boundaries 
+        // of the level.
+        var movement = Input.GetAxis("Horizontal");
+        movement *= (moveSpeed * Time.deltaTime);
+
+        var newPosition = transform.position;
+        newPosition.x += movement;
+        newPosition.x = Mathf.Clamp(newPosition.x, minPosition, maxPosition);
+
+        transform.position = newPosition;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckForNearbyNPC();
+        }
+    }
+
+    public void CheckForNearbyNPC()
+    {
+
+        
+    }
+}
+
